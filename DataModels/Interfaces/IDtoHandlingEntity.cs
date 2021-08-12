fd19ec4c00d139062257996ea0ec97d91c04338d
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2021-08-12 08:42:26 +0200

    tiny bit more notes

--- DataModels/Interfaces/IDtoHandlingEntity.cs
+++ DataModels/Interfaces/IDtoHandlingEntity.cs
@@ -11,7 +11,7 @@ namespace EntityFrameworkCoreTesting.DataModels.Interfaces
     /// </summary>
     /// <typeparam name="ModelType">The model of the entity.</typeparam>
     interface IDtoHandlingEntity <ModelType> //if this was an abstract class, it could be kind of like a factory design, I guess. It would not have the <ModelType>
-    { //The superclass contains the abstract method for creating, while the subclasses contain the actually implementation. Each subclass would return a result of their type rather than the superclass. There is a difference between Factory Design and Factory Method Design. Factory method design seems to be more my thing. These factory designs might be useful on a non-api webpage
+    { 
         /// <summary>
         /// Generates an entity out from the DTO.
         /// </summary>
