b40016b0b8ff7cd34a84f6a97972a1ed432cf06f
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2021-08-13 12:03:30 +0200

    working on testing something

---
+++ DataModels/Models/Test3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreTesting.DataModels.Models
+{
+    public class Test3
+    {
+        private HashSet<Test2> _test2s;
+        public string Test3Id { get; private set; }
+        public IEnumerable<Test2> Test2s { get => _test2s; private set => _test2s.ToHashSet(); }
+        private Test3()
+        {
+
+        }
+        public Test3(string id, HashSet<Test2> test2s = null)
+        {
+
+        }
+    }
+}
